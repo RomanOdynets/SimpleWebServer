b45d49fd1594d267385ace42f3eb4c68d1c9af71
Roman Odynets <r.chernenko28@gmail.com>
2021-05-16 11:44:20 +0300

    Ready

---
+++ SimpleWebServer/Core/Server.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SimpleWebServer.Core
+{
+    public class Server : IDisposable
+    {
+        private static List<Client> ClientsList { get; set; } = new();
+        
+        private readonly TcpListener _listener;
+        private Thread _listenerThread;
+        private const int Duration = 1;
+        private bool _isNotSuspended = true;
+
+        public Server() : this(80)
+        { }
+
+        public Server(ushort port)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);  
+            StartServer();
+        }
+        
+        public void StartServer()
+        {
+            _isNotSuspended = true;
+
+            if (_listenerThread is not null) return;
+            
+            _listener.Start();
+            _listenerThread = new Thread(ServerThread);
+            _listenerThread.IsBackground = true;
+            _listenerThread.Name = "Server Main Thread";
+            _listenerThread.Priority = ThreadPriority.AboveNormal;
+            _listenerThread.Start();
+        }
+
+        public void SuspendServer() => _isNotSuspended = false;
+
+        private void ServerThread()
+        {
+            while (_isNotSuspended)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), _listener.AcceptTcpClient());
+                Thread.Sleep(Duration);
+            }
+        }
+
+        private static void ClientThread([NotNull] object stateInfo)
+        {
+            var client = (TcpClient) stateInfo;
+            var serverClient = new Client(client);
+            ClientsList.Add(serverClient);
+        }
+
+        ~Server()
+        {
+            _listener?.Stop();
+        }
+
+        public void Dispose()
+        {
+            _isNotSuspended = false;
+            _listener.Stop();
+        }
+    }
+}
--- SimpleWebServer/Program.cs
+++ SimpleWebServer/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading;
+using SimpleWebServer.Core;
 
 namespace SimpleWebServer
 {
     class Program
     {
+        public const bool IsDebugEnabled = true;
+        public const string DefaultFileName = "index.html";
+        public const string FileFolder = "www";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var maxThreadsCount = Environment.ProcessorCount * 4;
+            ThreadPool.SetMaxThreads(maxThreadsCount, maxThreadsCount);
+            ThreadPool.SetMinThreads(2, 2);
+
+            var server = new Server();
+
+            Console.ReadLine();
         }
     }
 }
